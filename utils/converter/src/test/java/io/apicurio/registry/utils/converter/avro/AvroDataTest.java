55155aabe9676e9ba501bf2c816450f03545784b
spr0els <58633269+spr0els@users.noreply.github.com>
2025-06-11 15:08:13 +0200

    ZOLLAK2-615 cleanup unit unit tests for debezium temporal types avro conversion

--- utils/converter/src/test/java/io/apicurio/registry/utils/converter/avro/AvroDataTest.java
+++ utils/converter/src/test/java/io/apicurio/registry/utils/converter/avro/AvroDataTest.java
@@ -1,11 +1,11 @@
 package io.apicurio.registry.utils.converter.avro;
 
+import io.apicurio.registry.serde.avro.NonRecordContainer;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaAndValue;
 import org.apache.kafka.connect.data.SchemaBuilder;
-import org.apache.kafka.connect.data.Struct;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +17,6 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class AvroDataTest {
 
-    private static final String EXAMPLE_VALUE_NAME = "example_value";
     @Test
     public void testIntWithConnectDefault() {
         final String s = "{"
@@ -165,101 +164,91 @@ public class AvroDataTest {
     @Test
     public void testDebeziumTimestampType() {
         long debeziumTimestamp = io.debezium.time.Timestamp.toEpochMillis(new Date(), null);
-        String primitiveAvroType = "long";
-        org.apache.avro.Schema expectedAvroSchema = this.expectedAvroTypeSchema(io.debezium.time.Timestamp.SCHEMA_NAME, primitiveAvroType, "timestamp-millis");
 
-        SchemaAndValue outputSchemaValue = this.debeziumAvroToConnect(io.debezium.time.Timestamp.SCHEMA_NAME, primitiveAvroType, debeziumTimestamp);
-        Assertions.assertEquals(debeziumTimestamp, ((Struct) outputSchemaValue.value()).get(EXAMPLE_VALUE_NAME));
+        org.apache.avro.Schema expectedAvroSchema = expectedAvroTypeSchema(
+                io.debezium.time.Timestamp.SCHEMA_NAME,
+                "long",
+                "timestamp-millis");
 
-        GenericRecord outputRecord = this.genericRecordFromConnect(outputSchemaValue);
-        Assertions.assertTrue(this.isRecordValid(outputRecord, Long.class, expectedAvroSchema));
-        Assertions.assertEquals(debeziumTimestamp, outputRecord.get(EXAMPLE_VALUE_NAME));
+        SchemaAndValue connectValue = new SchemaAndValue(io.debezium.time.Timestamp.schema(), debeziumTimestamp);
+        AvroData avroData = new AvroData(0);
+        //noinspection unchecked
+        NonRecordContainer<Object> result = (NonRecordContainer<Object>) avroData.fromConnectData(connectValue.schema(), connectValue.value());
+
+        Assertions.assertEquals(expectedAvroSchema, result.getSchema());
+        Assertions.assertEquals(debeziumTimestamp, (long) result.getValue());
     }
 
     @Test
     public void testDebeziumDateType() {
         int epochDays = io.debezium.time.Date.toEpochDay(new Date(), null);
-        String primitiveAvroType = "int";
 
-        org.apache.avro.Schema expectedTypeSchema = this.expectedAvroTypeSchema(io.debezium.time.Date.SCHEMA_NAME, primitiveAvroType, "date");
-        SchemaAndValue outputSchemaValue = this.debeziumAvroToConnect(io.debezium.time.Date.SCHEMA_NAME, primitiveAvroType, epochDays);
-        Assertions.assertEquals(epochDays, (((Struct) outputSchemaValue.value()).get(EXAMPLE_VALUE_NAME)));
+        org.apache.avro.Schema expectedAvroSchema = expectedAvroTypeSchema(
+                io.debezium.time.Date.SCHEMA_NAME,
+                "int",
+                "date");
 
-        GenericRecord outputRecord = this.genericRecordFromConnect(outputSchemaValue);
-        Assertions.assertTrue(this.isRecordValid(outputRecord, Integer.class, expectedTypeSchema));
-        Assertions.assertEquals(epochDays, outputRecord.get(EXAMPLE_VALUE_NAME));
+        SchemaAndValue connectValue = new SchemaAndValue(io.debezium.time.Date.schema(), epochDays);
+        AvroData avroData = new AvroData(0);
+        //noinspection unchecked
+        NonRecordContainer<Object> result = (NonRecordContainer<Object>) avroData.fromConnectData(connectValue.schema(), connectValue.value());
+
+        Assertions.assertEquals(expectedAvroSchema, result.getSchema());
+        Assertions.assertEquals(epochDays, (int) result.getValue());
     }
-
-//    @Test
-//    public void testDebeziumMicroDurationType() {
-//        long durationMicros = MicroDuration.durationMicros(0, 0, 3, 4, 5, 6, null);
-//
-//        String expectedAvroSchemaString =
-//                "{" +
-//                "  \"type\" : \"fixed\"," +
-//                "  \"name\" : \"debeziumDuration\"," +
-//                "  \"size\" : 12," +
-//                "  \"connect.version\" : 1," +
-//                "  \"connect.name\" : \"" + MicroDuration.SCHEMA_NAME + "\"," +
-//                "  \"logicalType\" : \"duration\"" +
-//                "}";
-//
-//        org.apache.avro.Schema expectedTypeSchema = new org.apache.avro.Schema.Parser().parse(expectedAvroSchemaString);
-//        TimePeriod avroTimePeriod = TimePeriod.of(0, 3, 4*60*60*1_000 + 5*60*1_000 + 6*1_000);
-//        byte[] expectedAvroDurationFixedBytes = new Conversions.DurationConversion().toFixed(avroTimePeriod, expectedTypeSchema, LogicalTypes.duration()).bytes();
-//
-//        SchemaAndValue connectValue = new SchemaAndValue(MicroDuration.schema(), durationMicros);
-//        AvroData avroData = new AvroData(0);
-//        //noinspection unchecked
-//        NonRecordContainer<Byte[]> result = (NonRecordContainer<Byte[]>) avroData.fromConnectData(connectValue.schema(), connectValue.value());
-//
-//        Assertions.assertEquals(expectedTypeSchema, result.getSchema());
-//        Assertions.assertArrayEquals(expectedAvroDurationFixedBytes, (byte[]) result.getValue());
-//    }
 
     @Test
     public void testDebeziumMicroTimeType() {
         long microTime = io.debezium.time.MicroTime.toMicroOfDay(Duration.ofMinutes(5), true);
-        long microsSinceMidnight = 5*60*1_000*1_000;
-        String primitiveAvroType = "long";
 
-        org.apache.avro.Schema expectedTypeSchema = this.expectedAvroTypeSchema(io.debezium.time.MicroTime.SCHEMA_NAME, primitiveAvroType, "time-micros");
-        SchemaAndValue outputSchemaValue = this.debeziumAvroToConnect(io.debezium.time.MicroTime.SCHEMA_NAME, primitiveAvroType, microTime);
-        Assertions.assertEquals(microTime, (((Struct) outputSchemaValue.value()).get(EXAMPLE_VALUE_NAME)));
+        org.apache.avro.Schema expectedAvroSchema = expectedAvroTypeSchema(
+                io.debezium.time.MicroTime.SCHEMA_NAME,
+                "long",
+                "time-micros");
 
-        GenericRecord outputRecord = this.genericRecordFromConnect(outputSchemaValue);
-        Assertions.assertTrue(this.isRecordValid(outputRecord, Long.class, expectedTypeSchema));
-        Assertions.assertEquals(microsSinceMidnight, outputRecord.get(EXAMPLE_VALUE_NAME));
+        SchemaAndValue connectValue = new SchemaAndValue(io.debezium.time.MicroTime.schema(), microTime);
+        AvroData avroData = new AvroData(0);
+        //noinspection unchecked
+        NonRecordContainer<Object> result = (NonRecordContainer<Object>) avroData.fromConnectData(connectValue.schema(), connectValue.value());
+
+        Assertions.assertEquals(expectedAvroSchema, result.getSchema());
+        Assertions.assertEquals(microTime, (long) result.getValue());
     }
 
     @Test
     public void testDebeziumMicroTimestampType() {
-        Date now = new Date();
-        long microTimestamp = io.debezium.time.MicroTimestamp.toEpochMicros(now, null);
-        String primitiveAvroType = "long";
+        long microTimestamp = io.debezium.time.MicroTimestamp.toEpochMicros(new Date(), null);
 
-        org.apache.avro.Schema expectedTypeSchema = this.expectedAvroTypeSchema(io.debezium.time.MicroTimestamp.SCHEMA_NAME, primitiveAvroType, "timestamp-micros");
-        SchemaAndValue outputSchemaValue = this.debeziumAvroToConnect(io.debezium.time.MicroTimestamp.SCHEMA_NAME, primitiveAvroType, microTimestamp);
-        Assertions.assertEquals(microTimestamp, (((Struct) outputSchemaValue.value()).get(EXAMPLE_VALUE_NAME)));
+        org.apache.avro.Schema expectedAvroSchema = expectedAvroTypeSchema(
+                io.debezium.time.MicroTimestamp.SCHEMA_NAME,
+                "long",
+                "timestamp-micros");
 
-        GenericRecord outputRecord = this.genericRecordFromConnect(outputSchemaValue);
-        Assertions.assertTrue(this.isRecordValid(outputRecord, Long.class, expectedTypeSchema));
-        Assertions.assertEquals(microTimestamp, outputRecord.get(EXAMPLE_VALUE_NAME));
+        SchemaAndValue connectValue = new SchemaAndValue(io.debezium.time.MicroTimestamp.schema(), microTimestamp);
+        AvroData avroData = new AvroData(0);
+        //noinspection unchecked
+        NonRecordContainer<Object> result = (NonRecordContainer<Object>) avroData.fromConnectData(connectValue.schema(), connectValue.value());
+
+        Assertions.assertEquals(expectedAvroSchema, result.getSchema());
+        Assertions.assertEquals(microTimestamp, (long) result.getValue());
     }
 
     @Test
     public void testDebeziumTimeType() {
         int milliTime = io.debezium.time.Time.toMilliOfDay(Duration.ofMinutes(5), true);
-        int millisSinceMidnight = 5*60*1_000;
-        String primitiveAvroType = "int";
 
-        org.apache.avro.Schema expectedTypeSchema = this.expectedAvroTypeSchema(io.debezium.time.Time.SCHEMA_NAME, primitiveAvroType, "time-millis");
-        SchemaAndValue outputSchemaValue = this.debeziumAvroToConnect(io.debezium.time.Time.SCHEMA_NAME, primitiveAvroType, milliTime);
-        Assertions.assertEquals(milliTime, (((Struct) outputSchemaValue.value()).get(EXAMPLE_VALUE_NAME)));
+        org.apache.avro.Schema expectedAvroSchema = expectedAvroTypeSchema(
+                io.debezium.time.Time.SCHEMA_NAME,
+                "int",
+                "time-millis");
 
-        GenericRecord outputRecord = this.genericRecordFromConnect(outputSchemaValue);
-        Assertions.assertTrue(this.isRecordValid(outputRecord, Integer.class, expectedTypeSchema));
-        Assertions.assertEquals(millisSinceMidnight, outputRecord.get(EXAMPLE_VALUE_NAME));
+        SchemaAndValue connectValue = new SchemaAndValue(io.debezium.time.Time.schema(), milliTime);
+        AvroData avroData = new AvroData(0);
+        //noinspection unchecked
+        NonRecordContainer<Object> result = (NonRecordContainer<Object>) avroData.fromConnectData(connectValue.schema(), connectValue.value());
+
+        Assertions.assertEquals(expectedAvroSchema, result.getSchema());
+        Assertions.assertEquals(milliTime, (int) result.getValue());
     }
 
     private org.apache.avro.Schema expectedAvroTypeSchema(String connectName, String primitiveType, String logicalType) {
@@ -271,35 +260,5 @@ public class AvroDataTest {
                 "  \"logicalType\" : \"" + logicalType + "\"" +
                 "}";
         return new org.apache.avro.Schema.Parser().parse(typeSchemaString);
-    }
-
-    private <T> SchemaAndValue debeziumAvroToConnect(String connectName, String avroType, T debeziumValue){
-        final String AVROSCHEMA = "{\"type\": \"record\", " +
-                "\"name\": \"debezium_test\", " +
-                "\"connect.name\": \"io.apicurio.sample\", " +
-                "\"fields\": [" +
-                "   {\"type\": " +
-                "       [ " +
-                "           \"null\", " +
-                "           {\"type\": \"" + avroType + "\", \"connect.version\": 1, \"connect.name\": \"" + connectName + "\"}" +
-                "       ]," +
-                "      \"name\": \"" + EXAMPLE_VALUE_NAME + "\"" +
-                "   }" +
-                "]}";
-
-        AvroData avroData = new AvroData(0);
-        org.apache.avro.Schema avroSchema = new org.apache.avro.Schema.Parser().parse(AVROSCHEMA);
-        GenericRecord record = new GenericRecordBuilder(avroSchema).set(EXAMPLE_VALUE_NAME, debeziumValue).build();
-        return avroData.toConnectData(avroSchema, record);
-    }
-
-    private GenericRecord genericRecordFromConnect(SchemaAndValue connectSchemaValue){
-        AvroData avroData = new AvroData(0);
-        return (GenericRecord) avroData.fromConnectData(connectSchemaValue.schema(), connectSchemaValue.value());
-    }
-
-    private boolean isRecordValid(GenericRecord record, Class<?> expectedType, org.apache.avro.Schema expectedAvroSchema) {
-        return expectedType.isInstance(record.get(EXAMPLE_VALUE_NAME))
-                && record.getSchema().getField(EXAMPLE_VALUE_NAME).schema().getTypes().contains(expectedAvroSchema);
     }
 }
